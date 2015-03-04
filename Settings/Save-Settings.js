b6d488624001c6366dcd7462d8ba6fed75bda255
Josh-D-L <zepieguy@gmail.com>
2015-03-04 15:37:39 -0600

    Delete Save-Settings.js

--- Settings/Save-Settings.js
+++
@@ -1,9 +0,0 @@
-window.onbeforeunload = function() {
-  endSession();
-}
-function endSession() {
-  localStorage.setItem("Background Color", document.getElementById("Background Color").value);
-  localStorage.setItem("Title Color", document.getElementById("Title Color").value);
-  localStorage.setItem("Text Color", document.getElementById("Text Color").value);
-  localStorage.setItem("Font Size", document.getElementById("Font Size").value);
-}
