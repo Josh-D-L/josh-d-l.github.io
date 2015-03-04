058d3b6ff3c615fbfc8317ea5de6461afde61288
Josh-D-L <zepieguy@gmail.com>
2015-03-04 16:35:37 -0600

    Update Settings.js

--- Settings.js
+++ Settings.js
@@ -1,5 +1,4 @@
 document.getElementById("InjectTo").innerHTML = $('#Settings-Tab').contents().find("body").html();
-alert($("#Settings-Tab").contents().find('html').html());
 document.getElementsByClassName("spoiler")[0].style.display = "none";
 document.getElementById("Background Color").value = "#FFFFFF";
 var toggle = "on";
