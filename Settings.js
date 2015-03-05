e02a4a5c1e481934e041ab5884d57ba8c38c8be5
Josh-D-L <zepieguy@gmail.com>
2015-03-04 18:39:16 -0600

    Update Settings.js

--- Settings.js
+++ Settings.js
@@ -1,81 +1,78 @@
-window.onload=function(){
-	function endSession() {
-		  localStorage.setItem("Background Color", document.getElementById("Background Color").value);
-		  localStorage.setItem("Title Color", document.getElementById("Title Color").value);
-		  localStorage.setItem("Text Color", document.getElementById("Text Color").value);
-		  localStorage.setItem("Font Size", document.getElementById("Font Size").value);
+function endSession() {
+	  localStorage.setItem("Background Color", document.getElementById("Background Color").value);
+	  localStorage.setItem("Title Color", document.getElementById("Title Color").value);
+	  localStorage.setItem("Text Color", document.getElementById("Text Color").value);
+	  localStorage.setItem("Font Size", document.getElementById("Font Size").value);
+}
+var toggle = "on";
+function toggleDisplay() {
+	if (toggle == "on") {
+		document.getElementById("spoiler").style.display = "initial";
+		toggle = "off";
+	} else {
+		document.getElementById("spoiler").style.display = "none";
+		toggle = "on";
 	}
-	var toggle = "on";
-	function toggleDisplay() {
-		if (toggle == "on") {
-			document.getElementById("spoiler").style.display = "initial";
-			toggle = "off";
-		} else {
-			document.getElementById("spoiler").style.display = "none";
-			toggle = "on";
-		}
-	}
-	function hide() {
-		var wait = setTimeout(function() {
-			toggle = "on";
-			document.getElementById("spoiler").style.display = "none";
-		}, 3000);
-		document.getElementById("spoiler").addEventListener("mouseover", function() {
-			clearTimeout(wait);
-		}, false);
-	}
-	function reset() {
-		document.getElementById("Background Color").value = "#FFFFFF";
-		document.getElementById("Title Color").value = "#000000";
-		document.getElementById("Text Color").value = "#000000";
-		document.getElementById("Font Size").value = "16";
-		document.body.style.fontSize = 16 + "px";
-		document.getElementById("title").style.color = "#000000";
-		document.body.style.color = "#000000";
-		document.body.style.backgroundColor = "#FFFFFF";
-	}
-	function backgroundColor() {
-		var color = document.getElementById("Background Color").value;
-		document.body.style.backgroundColor = color;
-	}
-	function textColor() {
-		var color = document.getElementById("Text Color").value;
-		document.body.style.color = color;
-	}
-	function titleColor() {
-		var color = document.getElementById("Title Color").value;
-		document.getElementById("title").style.color = color;
-	}
-	function fontSize() {
-		var size = document.getElementById("Font Size").value;
-		document.body.style.fontSize = size + "px";
-	}
-	function settings() {
-		if (localStorage.getItem("Background Color") != null) {
-			document.getElementById("Background Color").value = localStorage.getItem("Background Color");
-		}
-		if (localStorage.getItem("Font Size") != null) {
-			document.getElementById("Font Size").value = localStorage.getItem("Font Size");
-			document.body.style.fontSize = localStorage.getItem("Font Size") + "px";
-		}
-		document.getElementById("Title Color").value = localStorage.getItem("Title Color");
-		document.getElementById("Text Color").value = localStorage.getItem("Text Color");
-		document.getElementById("title").style.color = localStorage.getItem("Title Color");
-		document.body.style.color = localStorage.getItem("Text Color");
-		document.body.style.backgroundColor = localStorage.getItem("Background Color");
-	}
-	if (reassign != undefined) {
-		reassign();
-	}
-	document.getElementsByClassName("spoiler")[0].style.display = "none";
+}
+function hide() {
+	var wait = setTimeout(function() {
+		toggle = "on";
+		document.getElementById("spoiler").style.display = "none";
+	}, 3000);
+	document.getElementById("spoiler").addEventListener("mouseover", function() {
+		clearTimeout(wait);
+	}, false);
+}
+function reset() {
 	document.getElementById("Background Color").value = "#FFFFFF";
-	settings();
-	window.onbeforeunload = function() {
-		endSession();
+	document.getElementById("Title Color").value = "#000000";
+	document.getElementById("Text Color").value = "#000000";
+	document.getElementById("Font Size").value = "16";
+	document.body.style.fontSize = 16 + "px";
+	document.getElementById("title").style.color = "#000000";
+	document.body.style.color = "#000000";
+	document.body.style.backgroundColor = "#FFFFFF";
+}
+function backgroundColor() {
+	var color = document.getElementById("Background Color").value;
+	document.body.style.backgroundColor = color;
+}
+function textColor() {
+	var color = document.getElementById("Text Color").value;
+	document.body.style.color = color;
+}
+function titleColor() {
+	var color = document.getElementById("Title Color").value;
+	document.getElementById("title").style.color = color;
+}
+function fontSize() {
+	var size = document.getElementById("Font Size").value;
+	document.body.style.fontSize = size + "px";
+}
+function settings() {
+	if (localStorage.getItem("Background Color") != null) {
+		document.getElementById("Background Color").value = localStorage.getItem("Background Color");
 	}
-	var host = "josh-d-l.github.io";
-	if ((host == window.location.host) && (window.location.protocol != "https:")) {
-		window.location.protocol = "https";
+	if (localStorage.getItem("Font Size") != null) {
+		document.getElementById("Font Size").value = localStorage.getItem("Font Size");
+		document.body.style.fontSize = localStorage.getItem("Font Size") + "px";
 	}
-	
-};
+	document.getElementById("Title Color").value = localStorage.getItem("Title Color");
+	document.getElementById("Text Color").value = localStorage.getItem("Text Color");
+	document.getElementById("title").style.color = localStorage.getItem("Title Color");
+	document.body.style.color = localStorage.getItem("Text Color");
+	document.body.style.backgroundColor = localStorage.getItem("Background Color");
+}
+if (reassign != undefined) {
+	reassign();
+}
+document.getElementsByClassName("spoiler")[0].style.display = "none";
+document.getElementById("Background Color").value = "#FFFFFF";
+settings();
+window.onbeforeunload = function() {
+	endSession();
+}
+var host = "josh-d-l.github.io";
+if ((host == window.location.host) && (window.location.protocol != "https:")) {
+	window.location.protocol = "https";
+}
